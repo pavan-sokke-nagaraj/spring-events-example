8a62aac14076239014b436e075de0decb0c877ae
Pavan Sokke Nagaraj <pavan.sokke.nagaraj@sap.com>
2018-01-22 20:46:49 -0500

    initial commit

---
+++ src/main/java/com/pa1/library/springevents/springeventsexample/SpringEventsExampleApplication.java
@@ -0,0 +1,12 @@
+package com.pa1.library.springevents.springeventsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEventsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEventsExampleApplication.class, args);
+	}
+}
